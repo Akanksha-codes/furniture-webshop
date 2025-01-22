073f1c77e535e0998b020db2befad32b27d98c50
Akanksha Gautam <68818469+Akanksha-codes@users.noreply.github.com>
2025-01-22 19:02:30 +0100

    Delete database/seed_data.sql

--- database/seed_data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO Products (product_id, product_name, description, base_price, category_id, weight_kg, dimension_cm, material_type,assembly_required, image_url, created_at, updated_at )
-VALUES
-    (01, 'Modern Sofa', 'Sofa', 'living room', 450, 2, '40kg', '200,80,90 cm', 'wood', TRUE, 'images/sofa.jpg', 18/01/2025 , 20/01/2025);
